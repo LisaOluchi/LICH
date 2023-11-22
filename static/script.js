f503af7b516a3b9e1b82a101609a20ca0a47a3a3
Lisa Sangqu <sangqulisa@gmail.com>
2023-11-22 01:48:04 +0000

    fixing

--- static/script.js
+++ static/script.js
@@ -1,65 +1,51 @@
-document.addEventListener('DocumentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
     const generateButton = document.getElementById('generate-outfit');
     const outfitDisplay = document.getElementById('outfit-display');
-
-    const clothingCategories =['Tops', 'Bottoms','Shoes'];
-    generateButton.addEventListener('click',generateOutfit);
-
-    function generateOutfit(){
-        const outfit = {};
-
-        clothingCategories.forEach(category=>{
-            const randomItem = getRandomItem (category);
-            outfit[category] = randomItem;
-        });
-
-        displayOutfit(outfit);
-    }
-
-    function getRandomItem(category) {
-        const items = {
-            'Tops': ['T-shirt', 'Blouse', 'Sweater'],
-            'Bottoms': ['Jeans', 'skirt', 'Shorts'],
-            'Shoes': ['Sneakers', 'Heels', 'Sildes'],
-        };
-        const randomIndex = Math.floor(Math.random() * itmes[category].length);
-        return items [category][randomIndex];
-    }
-
-    function displayOutfit(outfit){
-        let outfitHTML = '<strong>Your Random Outfit:</strong><br>';
-        for (const category in outfit) {
-            outfitHTML +=  `<p><em>${category}:</em> ${outfit[category]}</p>`;
-        }
-        outfitDisplay.innerHTML = outfitHTML;
-
-    }
-
     const randomOutfitButton = document.getElementById('random-outfit-button');
     const randomOutfitDisplay = document.getElementById('random-outfit-display');
 
+    const clothingCategories = ['Tops', 'Bottoms', 'Shoes'];
+
+    generateButton.addEventListener('click', generateOutfit);
     randomOutfitButton.addEventListener('click', generateRandomOutfit);
 
-    function generateRandomOutfit(){
-        const outfit = {}
+    function generateOutfit() {
+        const outfit = {};
 
         clothingCategories.forEach(category => {
             const randomItem = getRandomItem(category);
             outfit[category] = randomItem;
         });
 
-        displayRandomOutfit(outfit);
+        displayOutfit(outfit, outfitDisplay);
     }
 
-    function displayRandomOutfit(outfit) {
-        let randomOutfitHTML = '<strong>Your Random Outfit:</strong><br>';
+    function generateRandomOutfit() {
+        const outfit = {};
+
+        clothingCategories.forEach(category => {
+            const randomItem = getRandomItem(category);
+            outfit[category] = randomItem;
+        });
+
+        displayOutfit(outfit, randomOutfitDisplay);
+    }
+
+    function displayOutfit(outfit, displayElement) {
+        let outfitHTML = '<strong>Your Random Outfit:</strong><br>';
         for (const category in outfit) {
-            randomOutfitHTML += `<p><em>${category}:</em> ${outfit[category]}</p>`;
+            outfitHTML += `<p><em>${category}:</em> ${outfit[category]}</p>`;
         }
-        randomOutfitDisplay.innerHTML = randomOutfitHTML;
-
+        displayElement.innerHTML = outfitHTML;
     }
 
-
-
+    function getRandomItem(category) {
+        const items = {
+            'Tops': ['T-shirt', 'Blouse', 'Sweater'],
+            'Bottoms': ['Jeans', 'skirt', 'Shorts'],
+            'Shoes': ['Sneakers', 'Heels', 'Slides'],
+        };
+        const randomIndex = Math.floor(Math.random() * items[category].length);
+        return items[category][randomIndex];
+    }
 });
