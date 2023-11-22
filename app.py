b5e9403b6c5ee23c6607bcab33e025c68eed94f4
Lisa Sangqu <sangqulisa@gmail.com>
2023-11-22 06:18:48 +0000

    CHANGES

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
